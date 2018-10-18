33b030bcf494d4fadad71034f64584b142d60619
JamesCheng <chengcheng@youbanban.com>
2018-10-18 17:19:47 +0800

    Initial Commit

---
+++ Architecture/Architecture.h
@@ -0,0 +1,19 @@
+//
+//  Architecture.h
+//  Architecture
+//
+//  Created by JamesCheng on 2018/10/18.
+//  Copyright © 2018年 Molue. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Architecture.
+FOUNDATION_EXPORT double ArchitectureVersionNumber;
+
+//! Project version string for Architecture.
+FOUNDATION_EXPORT const unsigned char ArchitectureVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Architecture/PublicHeader.h>
+
+
